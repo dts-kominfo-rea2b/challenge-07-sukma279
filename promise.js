4e3c7a78d8909fc54a1c8d4ff2bf69416fe02a34
Sukma Suryaningsih <ingsih19@gmail.com>
2022-06-12 20:56:13 +0800

    menambahkan fungsi untuk menghitung emosi

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  const arrayIXX = await promiseTheaterIXX();
+  const arrayVGC = await promiseTheaterVGC();
+
+  const arrayTheater = [].concat(arrayIXX, arrayVGC);
+
+  const count = arrayTheater.filter((item) => item.hasil === emosi).length;
+
+  return count;
+};
 
 module.exports = {
   promiseOutput,
